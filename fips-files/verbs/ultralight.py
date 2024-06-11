8e196d54a4d54c0a0c7761685e7b16af82a2b440
Gustav Sterbrant <gustavsterbrant@dinas-mbp.localdomain>
2024-06-11 20:53:47 +0200

    More unpacking fuckery

--- fips-files/verbs/ultralight.py
+++ fips-files/verbs/ultralight.py
@@ -26,7 +26,7 @@ def download_ultralight(fips_dir, proj_dir):
     file_url = ULTRALIGHT_URL.format(ULTRALIGHT_RELEASE, ULTRALIGHT_RELEASE, platform_dict[util.get_host_platform()])
     target_dir = util.get_workspace_dir(fips_dir) + '/fips-build/ultralight-cache/'
     make_dirs(target_dir)
-    extract_dir = target_dir + 'extract'
+    extract_dir = target_dir + 'extract/'
     if os.path.isdir(extract_dir):
         shutil.rmtree(extract_dir, ignore_errors=True)
     make_dirs(extract_dir)
@@ -35,7 +35,7 @@ def download_ultralight(fips_dir, proj_dir):
     if not os.path.exists(target_file):
         log.info("downloading '{}'...".format(file_url))
         urlretrieve(file_url, target_file, None)
-    log.info("\nunpacking ... {}".format(target_file))
+    log.info("\nunpacking ... {} to {}".format(target_file, extract_dir))
     if sys.platform == "win32":
         import py7zr
         archive = py7zr.SevenZipFile(target_file)
